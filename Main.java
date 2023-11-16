2959288aed4894e32f0d165de96cbc15af21f783
muazsazelim <125224432+muazsazelim@users.noreply.github.com>
2023-11-16 01:38:33 +0000

    connection handler, main, and database operations

--- Main.java
+++ Main.java
@@ -1,1 +1,19 @@
+
 // Main.java
+import com.sheffield.DatabaseConnectionHandler;
+import com.sheffield.DatabaseOperations;
+
+public class Main {
+    public static void main(String[] args) {
+        DatabaseConnectionHandler databaseConnectionHandler = new DatabaseConnectionHandler();
+        DatabaseOperations databaseOperations = new DatabaseOperations();
+        try {
+            databaseConnectionHandler.openConnection();
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            databaseConnectionHandler.closeConnection();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# team014
+COM2008 System Designs and Security Assignments
+
+Team 014
+
+Members:
+1. Joshva Jerry
+2. Alcina Thomas Emili
+3. Mu'Az Bin Mohamad Nor Sazelim
+4. Muhammad Roizan
+
+
