bddee324ba52e1109d71b42b866c26b143de009c
Josh Jerry <jjerry1@sheffield.ac.uk>
2023-11-22 04:55:04 +0000

    Can now edit name and email

--- com/sheffield/views/UserDetailsView.java
+++ com/sheffield/views/UserDetailsView.java
@@ -7,6 +7,8 @@ import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import com.sheffield.util.TestOperations;
+
 
 public class UserDetailsView extends JFrame {
     private JLabel userDetails;
@@ -21,18 +23,97 @@ public class UserDetailsView extends JFrame {
         JPanel panel = new JPanel();
         this.add(panel);
 
-        this.getContentPane().setLayout(new BorderLayout());
+        JFrame parent = this;
+
+        parent.getContentPane().setLayout(new BorderLayout());
         panel.setLayout(new GridLayout(0,1));
 
+        
         // Create buttons that links to other pages from default page
         userDetails = new JLabel("Change Details");
-        
+
+        // Create JLabels for username and password
+        JLabel usernameLabel = new JLabel("  Username:");
+        JTextField usernameField = new JTextField(20);
+
+        // Create JTextFields for entering username and password
+
+
+        // Create a JButton for the login action
+        JButton searchButton = new JButton("Search");        
 
         // Add components to the panel
         panel.add(userDetails);
+        panel.add(usernameLabel);
+        panel.add(usernameField);
+        panel.add(searchButton);
 
-        this.getContentPane().add(panel, BorderLayout.NORTH);
-        this.pack();
+        parent.getContentPane().add(panel, BorderLayout.NORTH);
+        parent.pack();
+
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TestOperations testOperations = new TestOperations();
+                try {
+                    panel.add(new JLabel("Email - "+testOperations.getEmailByUsername(usernameField.getText(),connection)));
+                    JTextField updateEmail = new JTextField();
+                    panel.add(updateEmail);
+                    JButton updateEmailButton = new JButton("Update Email");
+                    panel.add(updateEmailButton);
+
+                    updateEmailButton.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            try {
+                                testOperations.updateEmailUsername(usernameField.getText(), updateEmail.getText(), connection);
+                            } catch (SQLException error) {
+                                System.out.println("SQL Error");
+                            }
+                        }
+                    });
+
+                    panel.add(new JLabel("Forename - "+testOperations.getForenameByUsername(usernameField.getText(),connection)));
+                    JTextField updateFName = new JTextField();
+                    panel.add(updateFName);
+                    JButton updateFNameButton = new JButton("Update Name");
+                    panel.add(updateFNameButton);
+
+                    updateFNameButton.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            try {
+                                testOperations.updateFNameUsername(usernameField.getText(), updateFName.getText(), connection);
+                            } catch (SQLException error) {
+                                System.out.println("SQL Error");
+                            }
+                        }
+                    });
+                    panel.add(new JLabel("Surname - "+testOperations.getSurnameByUsername(usernameField.getText(),connection)));
+                    JTextField updateSName = new JTextField();
+                    panel.add(updateSName);
+                    JButton updateSNameButton = new JButton("Update Name");
+                    panel.add(updateSNameButton);
+
+                    updateSNameButton.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            try {
+                                testOperations.updateSNameUsername(usernameField.getText(), updateSName.getText(), connection);
+                            } catch (SQLException error) {
+                                System.out.println("SQL Error");
+                            }
+                        }
+                    });
+                    panel.revalidate();
+                    panel.repaint();
+                    parent.pack();
+                    
+                } catch (SQLException error) {
+                    System.out.println("SQL Error");
+                }
+            }
+        });
 
     }
     
