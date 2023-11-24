fdaa18ff2f3d9eaba977578fca674b47b8dcea73
Alcina <athomasemili1@sheffield.ac.uk>
2023-11-24 04:07:00 +0000

    filtering for user order page

--- com/sheffield/util/OrderOperations.java
+++ com/sheffield/util/OrderOperations.java
@@ -10,12 +10,13 @@ import java.util.List;
 
 import com.sheffield.model.DatabaseConnectionHandler;
 import com.sheffield.model.order.Order;
+import com.sheffield.model.order.OrderLine;
 //import com.sheffield.model.DatabaseOperations;
 import com.sheffield.model.user.User;
 
 
 public class OrderOperations {
-    
+
     public Order[] getAllOrdersByUser (String userId, Connection connection) throws SQLException {
         try{
             String selectSQL = "SELECT * FROM Orders WHERE userId=?";
@@ -54,28 +55,20 @@ public class OrderOperations {
         
     }
 
-    public Order[] getAllOrdersLinesByOrder (String orderID, Connection connection) throws SQLException {
+    public OrderLine[] getAllOrdersLinesByOrder (int orderID, Connection connection) throws SQLException {
         try{
             String selectSQL = "SELECT * FROM Order_Lines WHERE orderID=?";
             PreparedStatement preparedStatement = connection.prepareStatement(selectSQL);
-            preparedStatement.setString(1, orderID);
+            preparedStatement.setInt(1, orderID);
             ResultSet resultSet = preparedStatement.executeQuery();
-            List<Order> orderList = new ArrayList<>();
+            List<OrderLine> orderList = new ArrayList<>();
             if (resultSet.next()) {
                
-                Order order = new Order(resultSet.getInt("orderID"),
-                                        resultSet.getString("userID"), 
-                                        resultSet.getDate("issueDate"),
-                                        resultSet.getDouble("totalCost"), 
-                                        resultSet.getString("status"));
-                
-                System.out.println("{" +
-                        "OrderId='" + resultSet.getInt("orderID") + "'" +
-                        ", userID='" + resultSet.getString("userID") + "'" +
-                        ", DATE='" + resultSet.getDate("issueDate") + "'" +
-                        ", cost='" +resultSet.getDouble("totalCost") + "'" +
-                        ",status'" + resultSet.getString("status") + "'" +
-                        "}");
+                OrderLine order = new OrderLine(resultSet.getInt("orderLineID"),
+                                        resultSet.getInt("orderID"), 
+                                        resultSet.getInt("productID"),
+                                        resultSet.getInt("quantity"), 
+                                        resultSet.getDouble("lineCost"));
                 orderList.add(order);
 
                 
@@ -83,7 +76,7 @@ public class OrderOperations {
                 System.out.println("");
             }
 
-            return orderList.toArray(new Order[0] );
+            return orderList.toArray(new OrderLine[0] );
         } catch (SQLException e){
             e.printStackTrace();
             throw e;
--- com/sheffield/util/TestOperations.java
+++ com/sheffield/util/TestOperations.java
@@ -323,43 +323,7 @@ public class TestOperations {
         }
     }
 
-    public Order[] getAllOrdersByUser (String userId, Connection connection) throws SQLException {
-        try{
-            String selectSQL = "SELECT * FROM Orders WHERE userId=?";
-            PreparedStatement preparedStatement = connection.prepareStatement(selectSQL);
-            preparedStatement.setString(1, userId);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            List<Order> orderList = new ArrayList<>();
-            if (resultSet.next()) {
-               
-                Order order = new Order(resultSet.getInt("orderID"),
-                                        resultSet.getString("userID"), 
-                                        resultSet.getDate("issueDate"),
-                                        resultSet.getDouble("totalCost"), 
-                                        resultSet.getString("status"));
-                
-                System.out.println("{" +
-                        "OrderId='" + resultSet.getInt("orderID") + "'" +
-                        ", userID='" + resultSet.getString("userID") + "'" +
-                        ", DATE='" + resultSet.getDate("issueDate") + "'" +
-                        ", cost='" +resultSet.getDouble("totalCost") + "'" +
-                        ",status'" + resultSet.getString("status") + "'" +
-                        "}");
-                orderList.add(order);
-
-                
-            } else {
-                System.out.println("");
-            }
-
-            return orderList.toArray(new Order[0] );
-        } catch (SQLException e){
-            e.printStackTrace();
-            throw e;
-        }
-
-        
-    }
+   
 
     public static void main(String[] args) {
         DatabaseConnectionHandler databaseConnectionHandler = new DatabaseConnectionHandler();
