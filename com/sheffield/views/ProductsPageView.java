9f3cc2ba3526bfafa86d144b2967d6ceff58fd87
muazsazelim <125224432+muazsazelim@users.noreply.github.com>
2023-11-30 23:52:29 +0000

    muaz

--- com/sheffield/views/ProductsPageView.java
+++ com/sheffield/views/ProductsPageView.java
@@ -3,13 +3,19 @@ package com.sheffield.views;
 import javax.swing.*;
 
 import com.sheffield.model.DatabaseConnectionHandler;
+import com.sheffield.model.order.Order;
 import com.sheffield.model.user.User;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import java.sql.*;
+import java.sql.Date;
 
 public class ProductsPageView extends JPanel {
 
@@ -26,16 +32,12 @@ public class ProductsPageView extends JPanel {
 
         // Create buttons that links to other pages from default page
 
-
-
-
         JButton trackPack = new JButton("Track Packs");
         JButton trackPiece = new JButton("Track Pieces");
         JButton locomotives = new JButton("Locomotives");
         JButton trainSet = new JButton("Train Sets");
         JButton rollingStock = new JButton("Rolling Stock");
         JButton controllers = new JButton("Controllers");
-
 
         navigation.add(trackPack);
         navigation.add(trackPiece);
@@ -44,6 +46,9 @@ public class ProductsPageView extends JPanel {
         navigation.add(rollingStock);
         navigation.add(controllers);
 
+        JButton userOrder = new JButton("My Cart");
+        navigation.add(userOrder);
+
         trackPack.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -51,13 +56,13 @@ public class ProductsPageView extends JPanel {
                 ProductsView productsView = null;
 
                 try {
-                    productsView = new ProductsView(connection, 1,user);
+                    productsView = new ProductsView(connection, 1, user);
 
                     productsView.setVisible(true);
 
                 } catch (Throwable t) {
                     throw new RuntimeException(t);
-                }                
+                }
 
             }
         });
@@ -69,13 +74,13 @@ public class ProductsPageView extends JPanel {
                 ProductsView productsView = null;
 
                 try {
-                    productsView = new ProductsView(connection, 2,user);
+                    productsView = new ProductsView(connection, 2, user);
 
                     productsView.setVisible(true);
 
                 } catch (Throwable t) {
                     throw new RuntimeException(t);
-                }                
+                }
 
             }
         });
@@ -87,13 +92,13 @@ public class ProductsPageView extends JPanel {
                 ProductsView productsView = null;
 
                 try {
-                    productsView = new ProductsView(connection, 3,user);
+                    productsView = new ProductsView(connection, 3, user);
 
                     productsView.setVisible(true);
 
                 } catch (Throwable t) {
                     throw new RuntimeException(t);
-                }                
+                }
 
             }
         });
@@ -105,13 +110,13 @@ public class ProductsPageView extends JPanel {
                 ProductsView productsView = null;
 
                 try {
-                    productsView = new ProductsView(connection, 4,user);
+                    productsView = new ProductsView(connection, 4, user);
 
                     productsView.setVisible(true);
 
                 } catch (Throwable t) {
                     throw new RuntimeException(t);
-                }                
+                }
 
             }
         });
@@ -123,13 +128,13 @@ public class ProductsPageView extends JPanel {
                 ProductsView productsView = null;
 
                 try {
-                    productsView = new ProductsView(connection, 5,user);
+                    productsView = new ProductsView(connection, 5, user);
 
                     productsView.setVisible(true);
 
                 } catch (Throwable t) {
                     throw new RuntimeException(t);
-                }                
+                }
 
             }
         });
@@ -141,13 +146,59 @@ public class ProductsPageView extends JPanel {
                 ProductsView productsView = null;
 
                 try {
-                    productsView = new ProductsView(connection, 6,user);
+                    productsView = new ProductsView(connection, 6, user);
 
                     productsView.setVisible(true);
 
                 } catch (Throwable t) {
                     throw new RuntimeException(t);
-                }                
+                }
+
+            }
+        });
+
+        userOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Went to Cart Page");
+                UserOrderView userO = null;
+
+                // Order userOrder = new Order();
+
+                try {
+                    String getQ = "SELECT * FROM Orders WHERE userId = '" + user.getuserId()
+                            + "' AND status = 'pending'";
+
+                    PreparedStatement getQS = connection.prepareStatement(getQ);
+                    ResultSet getQR = getQS.executeQuery();
+
+                    int orderID = 0;
+                    String userID = "";
+                    Date issueDate = Date.valueOf("2023-11-11");
+                    Double total = 0.00;
+                    String status = "pending";
+
+                    while (getQR.next()) {
+                        orderID = getQR.getInt("orderID");
+                        userID = getQR.getString("userId");
+                        // issueDate = getQR.getDate("issueDate");
+                        total = getQR.getDouble("totalCost");
+                    }
+
+                    if (orderID == 0) {
+                        JOptionPane.showMessageDialog(navigation, "Your cart is empty");
+                    } else {
+
+                        Order newOrder = new Order(orderID, userID, issueDate, total, status);
+
+                        userO = new UserOrderView(connection, newOrder, user);
+
+                        userO.setVisible(true);
+                    }
+
+                } catch (Throwable t) {
+                    throw new RuntimeException(t);
+                }
 
             }
         });
